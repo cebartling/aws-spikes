141fc8a25254926f525df4a1de74c11c32a764ac
Christopher Bartling <chris.bartling@gmail.com>
2022-04-21 10:45:21 -0500

    Adding stack source file

---
+++ aws-cdk-spike/hello-cdk/lib/hello-cdk-stack.ts
@@ -0,0 +1,13 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+
+export class HelloCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
+  }
+}
