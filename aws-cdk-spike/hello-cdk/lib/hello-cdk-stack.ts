e11fe4146377ef252d2e9839aec19c90838bc628
Christopher Bartling <chris.bartling@gmail.com>
2022-04-24 12:30:51 -0500

    feat: added hello Lambda function

--- aws-cdk-spike/hello-cdk/lib/hello-cdk-stack.ts
+++ aws-cdk-spike/hello-cdk/lib/hello-cdk-stack.ts
@@ -1,8 +1,15 @@
 import {Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
   }
 }
