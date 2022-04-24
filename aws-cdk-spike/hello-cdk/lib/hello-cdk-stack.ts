12eb872e2ddd348f8f9fec50dd5aea18b38c9111
Christopher Bartling <chris.bartling@gmail.com>
2022-04-24 15:58:07 -0500

    chore: add Prettier formatting

--- aws-cdk-spike/hello-cdk/lib/hello-cdk-stack.ts
+++ aws-cdk-spike/hello-cdk/lib/hello-cdk-stack.ts
@@ -1,5 +1,5 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
-import {Construct} from 'constructs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
