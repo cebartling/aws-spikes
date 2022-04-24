f5ac9963b356caadfd9c62326cf119d3331520b2
Christopher Bartling <chris.bartling@gmail.com>
2022-04-24 09:33:13 -0500

    chore: clean up all resources

--- aws-cdk-spike/hello-cdk/lib/hello-cdk-stack.ts
+++ aws-cdk-spike/hello-cdk/lib/hello-cdk-stack.ts
@@ -1,15 +1,8 @@
-import {RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
-    });
   }
 }
