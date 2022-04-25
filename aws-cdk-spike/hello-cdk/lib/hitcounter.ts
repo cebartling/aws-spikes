6fc45f8c3dc7d568c224491c5d9ac5b7f13abda5
Christopher Bartling <chris.bartling@gmail.com>
2022-04-25 08:29:13 -0500

    fix: refactoring to assign to readonly property

--- aws-cdk-spike/hello-cdk/lib/hitcounter.ts
+++ aws-cdk-spike/hello-cdk/lib/hitcounter.ts
@@ -18,10 +18,9 @@ export class HitCounter extends Construct {
     constructor(scope: Construct, id: string, props: HitCounterProps) {
         super(scope, id);
 
-        const table = new dynamodb.Table(this, 'Hits', {
+        this.table = new dynamodb.Table(this, 'Hits', {
             partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
         });
-        this.table = table;
 
         this.handler = new lambda.Function(this, 'HitCounterHandler', {
             runtime: lambda.Runtime.NODEJS_14_X,
@@ -29,12 +28,12 @@ export class HitCounter extends Construct {
             code: lambda.Code.fromAsset('lambda'),
             environment: {
                 DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-                HITS_TABLE_NAME: table.tableName
+                HITS_TABLE_NAME: this.table.tableName
             }
         });
 
         // grant the lambda role read/write permissions to our table
-        table.grantReadWriteData(this.handler);
+        this.table.grantReadWriteData(this.handler);
 
         // grant the lambda role invoke permissions to the downstream function
         props.downstream.grantInvoke(this.handler);
